9adc7df984be01c336dcf02803ebb1b1a916d85b
Clayton Moura <clee.gomes@gmail.com>
2024-07-29 18:11:27 -0300

    novo commit

--- src/test/java/me/dio/SantanderDevWeek2024ApplicationTests.java
+++ src/test/java/me/dio/SantanderDevWeek2024ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantanderDevWeek2023ApplicationTests {
+class SantanderDevWeek2024ApplicationTests {
 
 	@Test
 	void contextLoads() {
